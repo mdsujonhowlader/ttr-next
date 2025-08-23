01dfc27cf378309cf976e2089ac6e36622e3e1cc
Sujon <sujonhowlader67@gmail.com>
2025-08-23 23:25:44 +0600

    deplpyment issue solved

--- app/(main)/blogs/page.js
+++ app/(main)/blogs/page.js
@@ -1,5 +1,6 @@
 import { getBlogs } from "@/actions/BlogAction";
-import BlogHeroSection from "@/components/blog/blogHeroSection";
+import BlogHeroSection from "@/components/blog/BlogHeroSection";
+
 import BlogList from "@/components/blog/BlogList";
 
 export default async function BlogsPage() {
@@ -8,7 +9,6 @@ export default async function BlogsPage() {
   return (
     <section className="max-w-6xl mx-auto py-20">
       <BlogHeroSection />
-
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-16">
         <BlogList blogs={blogs} />
       </div>
